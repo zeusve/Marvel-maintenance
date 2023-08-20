999704ef270733ece1203e5656f72838cddc7c8f
Zeus Viso <zesfpv@gmail.com>
2023-08-20 17:33:55 +0200

    add data.sql

---
+++ Marvel/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.universo
+(nombre)
+VALUES('Marvel');
+
+INSERT INTO public.poder
+(nombre)
+VALUES('Fuerza');
+
+INSERT INTO public.super_heroe
+(live, nombre, universo_id)
+VALUES(false, 'Hulk', 1);
