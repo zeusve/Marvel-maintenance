630f2543310a508d5686727a7b0b2ea1bb7924d2
Zeus Viso <zesfpv@gmail.com>
2023-08-27 22:37:45 +0200

    update data file

--- Marvel/src/main/resources/data.sql
+++ Marvel/src/main/resources/data.sql
@@ -9,3 +9,27 @@ VALUES('Fuerza');
 INSERT INTO public.super_heroe
 (live, nombre, universo_id)
 VALUES(false, 'Hulk', 1);
+
+INSERT INTO public.universo
+(nombre)
+VALUES('DC');
+
+INSERT INTO public.poder
+(nombre)
+VALUES('Volar');
+
+INSERT INTO public.super_heroe
+(live, nombre, universo_id)
+VALUES(false, 'Superman', 1);
+
+INSERT INTO public.super_heroe_poderes
+(super_heroe_id, poderes_id)
+VALUES(1,1);
+
+INSERT INTO public.super_heroe_poderes
+(super_heroe_id, poderes_id)
+VALUES(2,1);
+
+INSERT INTO public.super_heroe_poderes
+(super_heroe_id, poderes_id)
+VALUES(2,2);
